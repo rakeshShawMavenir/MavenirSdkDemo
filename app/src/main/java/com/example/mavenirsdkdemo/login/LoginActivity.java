095c6eae06f48bdfbee15b04c82894590653558d
nalams <sridhar.nalam@mavenir.com>
2021-08-02 16:13:27 +0900

    Integrated Mavenir's RCS SDK dependency.

--- app/src/main/java/com/example/mavenirsdkdemo/login/LoginActivity.java
+++ app/src/main/java/com/example/mavenirsdkdemo/login/LoginActivity.java
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil;
 import com.example.mavenirsdkdemo.BaseActivity;
 import com.example.mavenirsdkdemo.R;
 import com.example.mavenirsdkdemo.databinding.ActivityLoginBinding;
+import com.mavenir.sdk.api.interfaces.ISDKManager;
 
 public class LoginActivity extends BaseActivity {
     ActivityLoginBinding mBinding;
@@ -16,5 +17,7 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        // TODO: Just to verify RCS SDK dependency integration.
+        ISDKManager.ACCOUNT_TYPE accountType = ISDKManager.ACCOUNT_TYPE.MULTIPLE;
     }
 }
